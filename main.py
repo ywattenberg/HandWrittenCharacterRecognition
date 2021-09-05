d1e2029586f7ecf6fb64cf4dbdfc2ba137b395d7
Yannick Wattenberg <wattenberg@mailbox.org>
2021-09-05 08:36:02 +0200

    trained new version

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-import numpy
-import pandas as pd
-import numpy as np
 import torch
-import matplotlib.pyplot as plt
 from characterdataset import CharacterDataset
 from model import Model
 from torch.utils.data import DataLoader
-
+from torchvision import transforms
+from PIL import Image
 
 def label_transform(z):
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -50,6 +47,24 @@ def test_loop(dataloader, model, loss_fn):
     print(f'Test Error \n Accuracy: {(100 * correct):>1f}%, Avg loss: {test_loss:>8f}')
 
 
+def recognize_character(pth):
+    alphabets_mapper = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H', 8: 'I', 9: 'J', 10: 'K',
+                        11: 'L', 12: 'M', 13: 'N', 14: 'O', 15: 'P', 16: 'Q', 17: 'R', 18: 'S', 19: 'T', 20: 'U',
+                        21: 'V', 22: 'W', 23: 'X', 24: 'Y', 25: 'Z'}
+    model = Model()
+    model.load_state_dict(torch.load('model_weights.pth', map_location='cpu'))
+    
+    if pth == "":
+        for i in range(1, 27):
+            img = Image.open(f'data/Images/Image-{i}.png').convert('L')
+            tensor = transforms.ToTensor()(img, ).apply_(lambda i: 1 - i).unsqueeze(0)
+            print(f'prediction: {alphabets_mapper[torch.argmax(model(tensor), 1).item()]} Actual: {alphabets_mapper[26-i]}')
+    else:
+        img = Image.open(pth).convert('L')
+        tensor = transforms.ToTensor()(img, ).apply_(lambda i: 1-i).unsqueeze(0)
+        print(f'prediction: {alphabets_mapper[torch.argmax(model(tensor), 1).item()]}')
+
+
 def main():
     learning_rate = 1e-3
     momentum = 0.9
@@ -84,4 +99,9 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    pre = input('[P]redict or [T]rain: ')
+    if pre == 'T' or pre == 't':
+        main()
+    else:
+        path = input('Input path to img: ')
+        recognize_character(path)
